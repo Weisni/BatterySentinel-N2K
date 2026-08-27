37544763324f4f541702a3e0ac63938abe1fbcff
Weisni <Marcel.Mainz@googlemail.com>
2026-08-27 23:41:16 +0200

    Support selectable INA238 shunt range and recovery

--- src/Ina238.h
+++ src/Ina238.h
@@ -9,28 +9,27 @@ namespace bs {
 
 class Ina238 {
 public:
-    Ina238(TwoWire& wire, uint8_t address, double shuntOhm)
-        : wire_(wire), address_(address), shuntOhm_(shuntOhm) {}
+    Ina238(TwoWire& wire, uint8_t address, double shuntOhm, bool narrowRange = false)
+        : wire_(wire), address_(address), shuntOhm_(shuntOhm), narrowRange_(narrowRange) {}
 
     bool begin();
     Measurement read();
     bool present() const { return present_; }
+    bool narrowRange() const { return narrowRange_; }
 
 private:
     static constexpr uint8_t REG_CONFIG = 0x00;
     static constexpr uint8_t REG_VSHUNT = 0x04;
     static constexpr uint8_t REG_VBUS = 0x05;
-
-    // INA238 ADCRANGE=0: signed shunt-voltage LSB = 5 uV/bit.
-    static constexpr double SHUNT_LSB_V = 5.0e-6;
-    // INA238 bus-voltage LSB = 3.125 mV/bit.
-    static constexpr double BUS_LSB_V = 3.125e-3;
+    static constexpr uint16_t CONFIG_ADCRANGE = 1u << 4;
 
     TwoWire& wire_;
     uint8_t address_;
     double shuntOhm_;
+    bool narrowRange_;
     bool present_ = false;
 
+    bool configure();
     bool ping();
     bool write16(uint8_t reg, uint16_t value);
     bool read16(uint8_t reg, uint16_t& value);
